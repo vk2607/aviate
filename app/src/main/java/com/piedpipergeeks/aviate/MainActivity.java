5c18ed6d98fe53648c8850135982e9269fb6ace4
shreyasgarud <shreyas.garud08@gmail.com>
2019-02-28 11:57:50 +0530

    Added club profile activity

--- app/src/main/java/com/piedpipergeeks/aviate/MainActivity.java
+++ app/src/main/java/com/piedpipergeeks/aviate/MainActivity.java
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i=new Intent(MainActivity.this,ClubDetails.class);
+        startActivity(i);
+
 
 //        startActivity(new Intent(MainActivity.this, PickClubActivity.class));
 
