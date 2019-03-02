66774a728dd126a39687a20e5be3815cabe2a149
shreyasgarud <shreyas.garud08@gmail.com>
2019-03-02 08:19:33 +0530

    Added create event activity

--- app/src/main/java/com/piedpipergeeks/aviate/MainActivity.java
+++ app/src/main/java/com/piedpipergeeks/aviate/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+startActivity(new Intent(MainActivity.this,CreateEvent.class));
         mAuth = FirebaseAuth.getInstance();
 
         fsClient = FirebaseFirestore.getInstance();
